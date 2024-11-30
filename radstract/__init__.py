5e2ed4a91239c73c076a66082f11c67138f7507f
Sharpz7 <adam.mcarthur62@gmail.com>
2024-11-30 08:50:52 +0000

    Added fix for pydicom log dumping

--- radstract/__init__.py
+++ radstract/__init__.py
@@ -1,3 +1,15 @@
 """
 .. include:: ../README.md
 """
+
+import logging
+
+
+class TileNumberFilter(logging.Filter):
+    def filter(self, record):
+        return "tile number" not in record.getMessage()
+
+
+# Replace 'your.logger.name' with the actual logger name
+logger = logging.getLogger("openjpeg.encode")
+logger.addFilter(TileNumberFilter())
